027c60d33783d78e801b0b1f03cab3f40b5d1e0b
parakh.pixel6 <parakh@pixel6.co>
2023-08-21 19:06:47 +0530

    Managing Data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   // Declarations or register component
@@ -25,6 +26,7 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +38,7 @@ import { CartComponent } from './cart/cart.component';
       // upr intital router mein single inverted comma ke bich space nhi rhena chahiye
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component:  CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ]),
     BrowserAnimationsModule
   ],
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -16,7 +16,7 @@ export class ProductDetailsComponent implements OnInit{
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService,
-    )  { }
+  )  { }
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
