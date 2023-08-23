667f18b2045b31148b4377fb5f30de4f64e6b9ad
parakh.pixel6 <parakh@pixel6.co>
2023-08-23 19:03:00 +0530

    cart form injected

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from "../cart.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-cart',
@@ -10,7 +11,21 @@ export class CartComponent {
 
   //This code sets the items using the CartService getItems() method. You defined this method  when u created cart.service.ts
   items = this.cartService.getItems( );
+
+  checkOutForm = this.formBuilder.group({
+    name: ' ',
+    address: ' ',
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+
   ) { }
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('You Order was placed successfully', this.checkOutForm.value);
+    this.checkOutForm.reset();
+  }
 }
